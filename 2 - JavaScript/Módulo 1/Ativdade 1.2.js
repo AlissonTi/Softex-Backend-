cf1823334f0e2159688a26e6d5261d9a50b3f1af
Alisson <alissondias.ti@gmail.com>
2023-08-29 16:34:45 -0300

    Inserção atividades 1.1 e 1.2 do javascript

---
+++ 2 - JavaScript/Módulo 1/Ativdade 1.2.js
@@ -0,0 +1,16 @@
+/*O meu ambiente escolhido foi o editor de texto vs code, por já conhecer um pouco dele e 
+por esse editor suportar várias liguagens de programação*/
+  
+let num1, num2, num3, media
+
+num1 = 7
+num2 = 9
+num3 = 10
+
+media = (num1 + num2 + num3)/3
+
+if(media > 7 ){
+    console.log("Aluno aprovado")
+}else{
+    console.log("Aluno de recuperação")
+}
