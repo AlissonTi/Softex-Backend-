a246d44c250b43a31ebc2bd270b2bcc192de3d58
Alisson <alissondias.ti@gmail.com>
2023-10-15 08:29:40 -0300

    Inserção atividade 4.1

--- 2 - JavaScript/Módulo 4/Atividade 4.1.js
+++ 2 - JavaScript/Módulo 4/Atividade 4.1.js
@@ -4,18 +4,23 @@ function Banco(conta, saldo, tipo, agencia){
     this.tipo = tipo
     this.agencia = agencia
 
-    this.buscarsaldo = function () {
-        console.log("SALDO DISPONÍVEL: " + this.saldo + " REAIS")
+    this.Buscarsaldo = function(){
+        console.log("SALDO DISPONÍVEL: " + saldo + " REAIS")
     }
-
-    function Deposito(){
-        
+    this.Deposito = function(valor = 50){
+        this.saldo = this.saldo + valor
+        console.log("SALDO DEPOIS DO DEPOSITO: " + this.saldo + " REAIS")
     }
-}   
-
+    this.Saque = function(valor = 50){
+        this.saldo = this.saldo - valor
+        console.log("SALDO DEPOIS DO SAQUE: " + this.saldo + " REAIS")
+    }
+    this.Numconta = function(){
+        console.log("CONTA NÚMERO: " + this.conta)
+    } 
+} 
 let conta1 = new Banco(1023, 200, "Conta Corrente", 51)
-console.log(conta1)
-conta1.buscarsaldo()
-
-
-
+conta1.Buscarsaldo()
+conta1.Deposito()
+conta1.Saque()
+conta1.Numconta()
